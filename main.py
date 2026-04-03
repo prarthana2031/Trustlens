19a0665a94ae47443ba5fef336eefe0dd4ae4650
Pallavi N <pallavin250575@gmail.com>
2026-04-03 20:53:15 +0530

    Added env variables safely

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
+#Environment variables
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 @app.get("/")
 def root():
-    return {"message": "Working!"}
+    return {"message": "Working!",
+            "supabase_loaded":SUPABASE URL is not None}
+
+
+
+
