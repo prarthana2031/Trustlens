574eaf2642bd2033635a0124033ffba34522d4c8
Pallavi-1506 <pallavin250575@gmail.com>
2026-04-07 21:31:22 +0530

    Update init.py

--- backend/app/init.py
+++ backend/app/init.py
@@ -1,7 +1,1 @@
-backend/__init__.py
-backend/app/__init__.py  
-backend/app/core/__init__.py
-backend/app/api/__init__.py
-backend/app/api/v1/__init__.py
-backend/app/middlewares/__init__.py
 """Backend application package"""
